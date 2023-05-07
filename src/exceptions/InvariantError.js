2e43eb10a53443eda5c7a88aa9645f767ac8d381
bagussurya12 <bagus.suryaindrajati@gmail.com>
2023-05-07 21:55:08 +0700

    update Handler JS

--- src/exceptions/InvariantError.js
+++ src/exceptions/InvariantError.js
@@ -1,10 +1,6 @@
 const ClientError = require("./clientError");
 
 class InvariantError extends ClientError {
-  constructor(message) {
-    super(message);
-    this.name = "InvariantError";
-  }
+  constructor(message) {}
 }
-
 module.exports = InvariantError;
