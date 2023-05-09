12b2622fb70bd3102026d5064fbca5866d2f6bd6
bagussurya12 <bagus.suryaindrajati@gmail.com>
2023-05-09 17:54:10 +0700

    upload

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
-
+//
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
